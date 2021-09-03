891908ee3313e88fff89cb77a5c743216bf62802
Quang <huuquang368@gmail.com>
2021-09-03 21:08:48 +0700

    get id user

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
 
-const io = require("socket.io")(server, {
+const io = require('socket.io')(server, {
   cors: {
-    origin: "*",
-    method: ["GET", "POST"],
+    origin: '*',
+    methods: ['GET', 'POST'],
   },
 });
 
@@ -13,21 +13,23 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.get('/', (req, res) => {
+  res.send('Running');
 });
 
-io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callended");
+io.on('connection', socket => {
+  socket.emit('me', socket.id);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
   });
-  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+
+  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit('callUser', { signal: signalData, from, name });
   });
-  //recieve data
-  socket.on("answercall", (data) => {
-    io.to(data.to).emit("callaccepted", data.signal);
+
+  socket.on('answerCall', data => {
+    io.to(data.to).emit('callAccepted', data.signal);
   });
 });
 
